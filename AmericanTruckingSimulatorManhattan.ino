a0910f1311f1e2deb94f5bd2cee96eaaa8f41449
FishPBL <139392964+FishPBL@users.noreply.github.com>
2023-08-24 21:20:29 -0600

    Add files via upload
    V2

--- AmericanTruckingSimulatorManhattan.ino
+++ AmericanTruckingSimulatorManhattan.ino
@@ -1,17 +1,25 @@
 #include <Keyboard.h>
 
+int dummy = 5;
+
 void setup() {
+
+pinMode(dummy, INPUT);
 
 }
 
 void loop() {
   
+if (digitalRead(dummy) == LOW) {
+
   Keyboard.press(KEY_KP_ENTER);
     delay(1000);
   Keyboard.press(KEY_KP_ENTER);
     delay(1000);
   Keyboard.press(KEY_KP_ENTER);
     delay(1000);
+
+  }
 
 }
 
