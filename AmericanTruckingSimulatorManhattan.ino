36d5e43c1bc1c7c81489362acaac9fef9fd8d764
FishPBL <139392964+FishPBL@users.noreply.github.com>
2023-08-24 20:57:45 -0600

    Add files via upload

---
+++ AmericanTruckingSimulatorManhattan.ino
@@ -0,0 +1,17 @@
+#include <Keyboard.h>
+
+void setup() {
+
+}
+
+void loop() {
+  
+  Keyboard.press(KEY_KP_ENTER);
+    delay(1000);
+  Keyboard.press(KEY_KP_ENTER);
+    delay(1000);
+  Keyboard.press(KEY_KP_ENTER);
+    delay(1000);
+
+}
+
